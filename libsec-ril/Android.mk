64f94a86ef677cfc741c4dff980adf7679a2e489
doadin <tbkizle@gmail.com>
2015-02-16 10:54:07 -0500

    Few Changes

--- libsec-ril/Android.mk
+++ libsec-ril/Android.mk
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Samsung-RIL.  If not, see <http://www.gnu.org/licenses/>.
 
+ifeq ($(BOARD_PROVIDES_LIBSEC-RIL),true)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -48,42 +50,9 @@ LOCAL_C_INCLUDES := \
 LOCAL_CFLAGS := -DRIL_SHLIB
 
 LOCAL_SHARED_LIBRARIES := libcutils libnetutils libutils liblog libpower
-LOCAL_STATIC_LIBRARIES := libsamsung-ipc
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libsec-ril
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := srs-client/srs-client.c
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/include \
-	$(LOCAL_PATH)/srs-client/include \
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-
-LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libsrs-client
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := tools/srs-test.c
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/include \
-	$(LOCAL_PATH)/srs-client/include \
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils libsrs-client
-
-LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := srs-test
-
-include $(BUILD_EXECUTABLE)
